44829dfed900c6bd823fd4694514e83facccbbe1
rokibulhasan <rokibulhasanutsoo@gmail.com>
2024-02-04 01:51:20 +0600

    chatting section update

--- src/pages/message/container/MessageSidebar.jsx
+++ src/pages/message/container/MessageSidebar.jsx
@@ -1,6 +1,6 @@
 import { memo, useEffect, useState } from "react";
 import ImageHeader from "../../../components/common/ImageHeader";
-import { child, get, limitToLast, onValue, query, ref } from "firebase/database";
+import { child, get, onValue, query, ref } from "firebase/database";
 import { db, uid } from "../../../firebase/realtimeDatabaseFunctions";
 import { Link } from "react-router-dom";
 import { format } from 'date-fns';
@@ -42,14 +42,14 @@ const MessageSidebar = () => {
                 const chatListArray = []
 
                 snapshot.forEach((eachItem) => {
-                    const chatId = eachItem.key.split('_').find((val) => val !== uid())
+                    const chatId = eachItem.key.split('_')
 
-                    if (chatId) {
+                    if (chatId.includes(uid())) {
                         const chatList = Object.values(eachItem.val())
                         const lastChatItem = chatList[chatList.length - 1]
                         
                         chatListArray.push({
-                            ...friendlist.find((val) => val.userId === chatId),
+                            ...friendlist.find((val) => val.userId === chatId.find((val) => val !== uid())),
                             ...lastChatItem
                         })
                     }
@@ -65,36 +65,39 @@ const MessageSidebar = () => {
 
     return (
         <div className="rounded-t-md flex flex-col justify-between h-full">
-            <div className="px-8 py-5 bg-white rounded-t-md overflow-hidden">
+            <div className="px-8 py-5 bg-white rounded-t-md overflow-hidden border-b">
                 <h2 className="text-2xl font-semibold">Messages</h2>
             </div>
 
-            <div className="flex-1 overflow-auto">
+            <div className="flex-1 overflow-x-auto">
             {
                 chatList?.map((val) => (
                     <Link key={val.id} to={`/messages/to/${val.userId}`}>
-                        <div key={val.id} className="relative px-8 py-4 border-y hover:bg-white hover:after:absolute after:rounded-tr-md after:rounded-br-md after:bg-app-primary after:w-1 after:h-[calc(100%+2px)] after:z-10 after:-top-px after:-left-[2px]">
-                            <div className="flex items-center space-x-3 select-none py-0.5 transition-all">
+                        <div className="relative border-b hover:after:absolute after:rounded-tr-md after:rounded-br-md after:bg-app-primary after:w-1.5 after:h-[calc(100%+2px)] after:z-50 after:-top-px after:-left-[2px]">
+                            <div className="flex gap-x-4 px-8 py-4 bg-white rounded-xl">
                                 {/* head image */}
                                 <ImageHeader size={'sm'} photoUrl={val?.imgUrl} name={val?.name}/>
-                                {/* {console.log(val)} */}
+                                
+                                <div className="flex-1">
+                                    <div className="flex items-center space-x-3 select-none py-0.5 transition-all">
+                                        <div className="flex-1">
+                                            <p className="font-semibold text-xl">{val?.name}</p>
+                                        </div>
+                                        <div className="items-stretch my-auto">
+                                            <span className="text-sm py-1.5 text-slate-500">{format(new Date(val.date), 'p')}</span>
+                                        </div>
+                                    </div>
 
-                                <div className="px-2 py-1 flex-1">
-                                    <p className="font-semibold text-2xl">{val?.name}</p>
-                                </div>
-                                <div className="items-stretch my-auto">
-                                    <span className="text-sm py-1.5 text-slate-500">{format(new Date(val.date), 'p')}</span>
+                                    {/* message content */}
+                                    <p className="text-base leading-normal tracking-tight text-slate-600 whitespace-pre-line line-clamp-2">
+                                    {
+                                        val?.msg_react
+                                        ? <span>{val.senderId !== uid() ? `Reacted ${val?.msg_react} to your message` : ''}</span>
+                                        : <span>{`${val.senderId === uid() ? 'You:' : ''} ${val?.message}`}</span>
+                                    }
+                                    </p>
                                 </div>
                             </div>
-
-                            {/* message content */}
-                            <p className="text-base mt-2 leading-normal tracking-tight py-1 text-slate-600 whitespace-pre-line line-clamp-2">
-                                {
-                                    val?.msg_react
-                                    ? <span>Reacted {val?.msg_react} to your message</span>
-                                    : <span>{`${val.senderId === uid() ? 'You:' : ''} ${val?.message}`}</span>
-                                }
-                            </p>
                         </div>
                     </Link>
                 ))
